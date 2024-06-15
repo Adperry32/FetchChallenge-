0c4b16a06cc28518dc07d5cef7000e433d7b7b4c
Arrington Perry <perryceo21@hotmail.com>
2024-06-14 21:53:46 -0400

    Data class for items created and adapter setup.

---
+++ app/src/main/java/com/example/fetch_codetest/Items.java
@@ -0,0 +1,22 @@
+package com.example.fetch_codetest;
+
+public class Items {
+    private int id;
+    private int listId;
+    private String name;
+    public int getId() {
+        return id;
+    }
+
+    public int getListId() {
+        return listId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+
+
+}
---
+++ app/src/main/java/com/example/fetch_codetest/RecycleViewAdapter.java
@@ -0,0 +1,58 @@
+package com.example.fetch_codetest;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RecycleViewAdapter extends RecyclerView.Adapter<RecycleViewAdapter.ItemViewHolder> {
+
+    private List<Items> _list = new ArrayList<>();
+    public void apiList(List<Items> items){
+        _list = items;
+        notifyDataSetChanged();
+    }
+    @NonNull
+    @Override
+    public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_cardview_layout, parent, false);
+        return new ItemViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecycleViewAdapter.ItemViewHolder holder, int position) {
+
+        Items item = _list.get(position);
+        holder.bind(item);
+    }
+
+    @Override
+    public int getItemCount() {
+        return _list.size();
+    }
+    public static class ItemViewHolder extends RecyclerView.ViewHolder {
+        private final TextView nameTextView;
+        private final TextView listIdTextView;
+
+        public ItemViewHolder(@NonNull View itemView) {
+            super(itemView);
+            
+            listIdTextView = itemView.findViewById(R.id.itemListId);
+            nameTextView = itemView.findViewById(R.id.itemName);
+        }
+
+        public void bind(Items item) {
+
+            listIdTextView.setText(String.valueOf(item.getListId()));
+            nameTextView.setText(item.getName());
+        }
+    }
+}
+
+
