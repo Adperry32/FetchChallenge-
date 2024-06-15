58750c499725466f4d8601618f303320f33fb80d
Arrington Perry <perryceo21@hotmail.com>
2024-06-14 21:26:00 -0400

    Transition to fragment added and cardview with recyclerview created. With animation to button functionality.

--- app/src/main/java/com/example/fetch_codetest/MainActivity.java
+++ app/src/main/java/com/example/fetch_codetest/MainActivity.java
@@ -9,6 +9,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 
 import androidx.core.view.WindowCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -18,18 +21,46 @@ import com.example.fetch_codetest.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
-
     private ActivityMainBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
+
         setContentView(binding.getRoot());
+
+        //adding function to and animations to button
+
+        Animation pulseAnimation = AnimationUtils.loadAnimation(this, R.anim.pulsate);
+        binding.fetchDataButton.startAnimation(pulseAnimation);
+
+
+        //listen for click to initiate transition to fragment
+        binding.fetchDataButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Begin the fragment transaction
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+                // Replace the existing  container (R.id.mainActivity) with the fragment
+                transaction.replace(R.id.main_Activity, new ViewDataFragment());
+
+                // Add the transaction to the back stack (optional, for back navigation)
+                transaction.addToBackStack(null);
+
+                // Commit the transaction
+                transaction.commit();
+            }
+        });
     }
 
     @Override
@@ -51,8 +82,13 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
 
+        if(id == R.id.action_exit){
+
+        }
+
         return super.onOptionsItemSelected(item);
     }
+
 
 
 }
---
+++ app/src/main/java/com/example/fetch_codetest/ViewDataFragment.java
@@ -0,0 +1,24 @@
+package com.example.fetch_codetest;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class ViewDataFragment extends Fragment {
+
+    public static final String TAG = "ViewDataFragment";
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_view_data, container, false);
+        return view;
+
+    }
+}
